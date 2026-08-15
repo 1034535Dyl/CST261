85ffc7e8c604ed119b43c04c156791db87fd6b9d
Dylan Lindley <1034535@student.sae.edu.au>
2026-08-15 15:08:31 +1000

    Initial commit

---
+++ datastuff.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+
+
+# function to load the data set into work space
+HOUSING_PATH = os.path.join("datasets", "housing")
+def load_housing_data(housing_path=HOUSING_PATH):
+
+    csv_path = os.path.join(housing_path, "housing.csv")
+
+    return pd.read_csv(csv_path)
+
+housing
